c8bad768d835d84b3271eb815043103d67887607
nisan92 <nisan92@yahoo.com>
2014-03-05 12:52:42 +0600

    secound commit
    added jquery.js

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
             </div>
         </div>
         
+        <script src="js/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
-        <script src="js/bootstrap.js"></script>
     </body>
 </html>
 
