b72b8bb1ea735d4480c4905db1e455a3e7c06ee8
nisan92 <nisan92@yahoo.com>
2014-03-05 12:27:43 +0600

    init release
    init release

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AFD </title>
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+        <link href="css/custom.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        
+        
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </body>
+</html>
+
