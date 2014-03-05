4dafee8eeb428dfa0607a564c473a21e4154e025
nisan92 <nisan92@yahoo.com>
2014-03-05 13:36:15 +0600

    3rd commit
    Added heading and forms

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML>
+﻿<!DOCTYPE HTML>
 <html lang="en">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -21,7 +21,66 @@
                     <img src="img/pic 03.jpg" alt="" class="img-responsive hidden-xs"/>
                 </div>
             </div>
-        </div>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 center-align">
+                    <h2>এ.এফ.ডি সিস্টেমে স্বাগতম  </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="ofwhite">
+                    <div class="col-md-6">
+                        <h2>সর্বশেষ সংবাদ</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="right-align">
+                            <h2>লগ ইন</h2>
+                        </div>
+                        <form class="form-horizontal" role="form">
+                            <div class="form-group">
+                                <label for="lavel" class="col-sm-2 control-label">পর্যায়ঃ  </label>
+                                <div class="col-sm-6">
+                                    <select class="form-control">
+                                        <option sele>বাছাই করুন </option>
+                                        <option>ইউনিয়ন </option>
+                                        <option>উপজেলা </option>
+                                        <option>জেলা</option>
+                                        <option>ডিসি </option>
+                                        <option>মন্ত্রণালয় </option>
+                                    </select>
+                                </div>
+                            </div>
+                                    <div class="form-group">
+                                        <label for="username" class="col-sm-2 control-label">ইউজার আইডিঃ </label>
+                                        <div class="col-sm-6">
+                                            <input type="text" class="form-control" id="username" placeholder="username">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="inputPassword3" class="col-sm-2 control-label">পাসওয়ার্ডঃ </label>
+                                        <div class="col-sm-6">
+                                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-offset-2 col-sm-6">
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" /> মনে রাখুন
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-offset-2 col-sm-6">
+                                            <button type="submit" class="btn btn-default">Sign in</button>
+                                        </div>
+                                    </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                    </div>
+                </div>
         
         <script src="js/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
